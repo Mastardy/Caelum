52f6e672452f86b0e99b028604ad13a745c8a46a
André <andremgmonteiro01@gmail.com>
2022-03-04 14:05:15 +0000

    Steamworks Golden Path

---
+++ Assets/Scripts/GameNetworkManager.cs
@@ -0,0 +1,208 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Netcode.Transports.Facepunch;
+using Steamworks;
+using Steamworks.Data;
+using Unity.Netcode;
+using UnityEngine;
+
+public class GameNetworkManager : MonoBehaviour
+{
+    public static GameNetworkManager Singleton { get; private set; }
+    public Lobby? CurrentLobby { get; set; }
+    public List<Lobby> Lobbies { get; private set; } = new(capacity: 100);
+    
+    private FacepunchTransport transport;
+
+    private void OnGUI()
+    {
+        GUILayout.BeginArea(new Rect(10, 10, 300, 300));
+        if (!NetworkManager.Singleton.IsClient && !NetworkManager.Singleton.IsServer)
+        {
+            StartButtons();
+        }
+            
+        GUILayout.EndArea();
+    }
+
+    private static void StartButtons()
+    {
+        if (GUILayout.Button("Host")) GameNetworkManager.Singleton.StartHost();
+    }
+    
+    private void Awake()
+    {
+        if(Singleton == null) Singleton = this;
+        else
+        {
+            Destroy(gameObject);
+            return;
+        }
+        
+        DontDestroyOnLoad(gameObject);
+    }
+
+    private void Start()
+    {
+        transport = GetComponent<FacepunchTransport>();
+
+        SteamMatchmaking.OnLobbyCreated += OnLobbyCreated;
+        SteamMatchmaking.OnLobbyEntered += OnLobbyEntered;
+        SteamMatchmaking.OnLobbyMemberJoined += OnLobbyMemberJoined;
+        SteamMatchmaking.OnLobbyMemberLeave += OnLobbyMemberLeave;
+        SteamMatchmaking.OnLobbyInvite += OnLobbyInvite;
+
+        SteamFriends.OnGameLobbyJoinRequested += OnGameLobbyJoinRequested;
+    }
+
+    public async void StartHost()
+    {
+        NetworkManager.Singleton.OnClientConnectedCallback += OnClientConnectedCallback;
+        NetworkManager.Singleton.OnClientDisconnectCallback += OnClientDisconnectCallback;
+        NetworkManager.Singleton.OnServerStarted += OnServerStarted;
+        
+        NetworkManager.Singleton.StartHost();
+
+        CurrentLobby = await SteamMatchmaking.CreateLobbyAsync(4);
+    }
+
+    public void StartClient(SteamId id)
+    {
+        NetworkManager.Singleton.OnClientConnectedCallback += OnClientConnectedCallback;
+        NetworkManager.Singleton.OnClientDisconnectCallback += OnClientDisconnectCallback;
+
+        transport.targetSteamId = id;
+        
+        Debug.Log($"Joining room hosted by {transport.targetSteamId}", this);
+
+        if (NetworkManager.Singleton.StartClient()) Debug.Log("Client has joined!", this);
+    }
+
+    private void Disconnect()
+    {
+        CurrentLobby?.Leave();
+        
+        if (NetworkManager.Singleton == null) return;
+        
+        NetworkManager.Singleton.Shutdown();
+    }
+
+    public async Task<bool> RefreshLobbies(int maxResults = 20)
+    {
+        try
+        {
+            Lobbies.Clear();
+
+            var lobbies = await SteamMatchmaking.LobbyList
+                .FilterDistanceClose()
+                .WithMaxResults(maxResults)
+                .RequestAsync();
+
+            if (lobbies != null)
+            {
+                for (int i = 0; i < lobbies.Length; i++) Lobbies.Add(lobbies[i]);
+            }
+
+            return true;
+        }
+        catch (System.Exception ex)
+        {
+            Debug.Log("Error fetching lobbies", this);
+            Debug.LogException(ex, this);
+            return false;
+        }
+    }
+
+    private Steamworks.ServerList.Internet GetInternetRequest()
+    {
+        var request = new Steamworks.ServerList.Internet();
+
+        return request;
+    }
+    
+    #region NetworkCallbacks
+
+    private void OnServerStarted() => Debug.Log("Server has started!");
+
+    private void OnClientConnectedCallback(ulong clientId) => Debug.Log($"Client connected, clientId = {clientId}");
+
+    private void OnClientDisconnectCallback(ulong clientId)
+    {
+        Debug.Log($"Client disconnected, clientId = {clientId}");
+     
+        NetworkManager.Singleton.OnClientConnectedCallback -= OnClientConnectedCallback;
+        NetworkManager.Singleton.OnClientDisconnectCallback -= OnClientDisconnectCallback;
+    }
+
+    #endregion
+    
+    #region SteamCallbacks
+
+    private void OnLobbyCreated(Result result, Lobby lobby)
+    {
+        if (result != Result.OK)
+        {
+            Debug.LogError($"Lobby couldn't be created!, {result}", this);
+            return;
+        }
+        
+        lobby.SetPublic();
+        lobby.SetData("name", "Cool Lobby");
+        lobby.SetJoinable(true);
+
+        Debug.Log("Lobby has been created!");
+    }
+
+    private void OnLobbyEntered(Lobby lobby)
+    {
+        Debug.Log($"You have entered in lobby, clientId = {NetworkManager.Singleton.LocalClientId}", this);
+        
+        if (NetworkManager.Singleton.IsHost) return;
+        
+        StartClient(lobby.Owner.Id);
+    }
+
+    private void OnLobbyMemberJoined(Lobby lobby, Friend friend)
+    {
+        Debug.Log("On Lobby Member Joined");
+    }
+
+    private void OnLobbyMemberLeave(Lobby lobby, Friend friend)
+    {
+        Debug.Log("On Lobby Member Leave");
+    }
+
+    private void OnLobbyInvite(Friend friend, Lobby lobby) => Debug.Log($"You got an invite from {friend.Name}");
+
+    private void OnGameLobbyJoinRequested(Lobby lobby, SteamId steamId)
+    {
+        Debug.Log("On Game Lobby Join Requested");
+
+        bool isSame = lobby.Owner.Id.Equals(steamId);
+        
+        Debug.Log($"Owner: {lobby.Owner}");
+        Debug.Log($"Id: {steamId}");
+        Debug.Log($"IsSame: {isSame}");
+        
+        StartClient(steamId);
+    }
+
+    #endregion
+
+    private void OnDestroy()
+    {
+        SteamMatchmaking.OnLobbyCreated -= OnLobbyCreated;
+        SteamMatchmaking.OnLobbyEntered -= OnLobbyEntered;
+        SteamMatchmaking.OnLobbyMemberJoined -= OnLobbyMemberJoined;
+        SteamMatchmaking.OnLobbyMemberLeave -= OnLobbyMemberLeave;
+        SteamMatchmaking.OnLobbyInvite -= OnLobbyInvite;
+
+        if (NetworkManager.Singleton == null) return;
+
+        NetworkManager.Singleton.OnClientConnectedCallback -= OnClientConnectedCallback;
+        NetworkManager.Singleton.OnClientDisconnectCallback -= OnClientDisconnectCallback;
+        NetworkManager.Singleton.OnServerStarted -= OnServerStarted;
+    }
+
+    private void OnApplicationQuit() => Disconnect();
+}
