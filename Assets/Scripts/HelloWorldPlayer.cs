d78043040c0edff669cf605fbd1d180506ee80dc
André <andremgmonteiro01@gmail.com>
2022-03-04 00:11:31 +0000

    Fixed Facepunch.Steamworks

--- Assets/Scripts/HelloWorldPlayer.cs
+++ Assets/Scripts/HelloWorldPlayer.cs
@@ -8,11 +8,11 @@ namespace HelloWorld
         private void Update()
         {
             var speed = 3 * Time.deltaTime;
-            
-            transform.position += Input.GetKey(KeyCode.W) ? transform.forward * speed : 
-                Input.GetKey(KeyCode.D) ? transform.right * speed : 
-                Input.GetKey(KeyCode.S) ? -transform.forward * speed :
-                Input.GetKey(KeyCode.A) ? -transform.right * speed : Vector3.zero;
+
+            transform.position += Input.GetKey(KeyCode.W) ? transform.forward * speed : Vector3.zero;
+            transform.position += Input.GetKey(KeyCode.D) ? transform.right * speed : Vector3.zero;
+            transform.position += Input.GetKey(KeyCode.S) ? -transform.forward * speed : Vector3.zero;
+            transform.position += Input.GetKey(KeyCode.A) ? -transform.right * speed : Vector3.zero;
         }
     }
 }
