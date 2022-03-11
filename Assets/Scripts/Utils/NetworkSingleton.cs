720104b0a46fb654451493e71516c1d9b9c7d99c
André <andremgmonteiro01@gmail.com>
2022-03-11 18:30:54 +0000

    Singleton Util (Networked/Non-Networked)

---
+++ Assets/Scripts/Utils/NetworkSingleton.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+using Unity.Netcode;
+
+public class NetworkSingleton<T> : NetworkBehaviour where T : NetworkSingleton<T>
+{
+    public static T Instance { get; private set; }
+    public static bool IsInitialized => Instance != null;
+
+    protected virtual void Awake()
+    {
+        if (IsLocalPlayer)
+        {
+            if (Instance != null) Debug.LogError("[Singleton] Possible duplicate of a singleton: " + typeof(T));
+            else Instance = (T)this;
+        }
+    }
+
+    public override void OnDestroy()
+    {
+        if (IsLocalPlayer)
+        {
+            if (Instance == this) Instance = null;
+        }
+    }
+}
