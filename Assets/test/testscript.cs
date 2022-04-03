a6411b7dd0d7958898f7b811ee54c33ecb8c1257
Lucas <lucas_cmattos@hotmail.com.br>
2022-04-03 14:13:10 +0100

    Added jump and pitch animations

--- Assets/test/testscript.cs
+++ Assets/test/testscript.cs
@@ -8,6 +8,10 @@ public class testscript : MonoBehaviour
     [Range(0, 1)]public float speed = 0;
     [Range(-180,180)] public float direction = 0;
     public bool crouch = false;
+    public bool jump = false;
+    [Range(-90, 90)] public float pitch = 0;
+
+    public Vector3 original_position = Vector3.zero;
 
     // Update is called once per frame
     void Update()
@@ -15,6 +19,10 @@ public class testscript : MonoBehaviour
         animator.SetFloat("Speed", speed);
         animator.SetFloat("Direction", map(direction, -180, 180));
         animator.SetBool("Crouch", crouch);
+        animator.SetBool("Jump", jump);
+        animator.SetFloat("Pitch", map(pitch, -90, 90));
+
+        //transform.position = new Vector3(Mathf.Lerp(-5, -8, Mathf.Sin(Time.time*2)), original_position.y, original_position.z);
     }
 
     // Maps a value from some arbitrary range to the 0 to 1 range
