bd719ef543d430c1f6bef6b66597e8d31ea4d8bc
Lucas <lucas_cmattos@hotmail.com.br>
2022-04-06 15:30:28 +0100

    Added some fps animations
    Running, Crouch and Jump

--- Assets/Scripts/Player/Player.Animator.cs
+++ Assets/Scripts/Player/Player.Animator.cs
@@ -9,6 +9,7 @@ public partial class Player
     [SerializeField] private Animator firstPersonAnimator;
     
     private static readonly int speedCache = Animator.StringToHash("Speed");
+    private static readonly int sprintCache = Animator.StringToHash("Sprint");
     private static readonly int directionCache = Animator.StringToHash("Direction");
     private static readonly int crouchCache = Animator.StringToHash("Crouch");
     private static readonly int jumpCache = Animator.StringToHash("Jump");
@@ -25,13 +26,16 @@ public partial class Player
 
     private void AnimatorStart()
     {        
-        //dar Hide no world model mantendo as sombras
         if (IsLocalPlayer)
         {
-            foreach(var smr in thirdPersonModel.GetComponentsInChildren<SkinnedMeshRenderer>())
+            //dar Hide no world model mantendo as sombras
+            foreach (var smr in thirdPersonModel.GetComponentsInChildren<SkinnedMeshRenderer>())
             {
                 smr.shadowCastingMode = UnityEngine.Rendering.ShadowCastingMode.ShadowsOnly;
             }
+            
+            //mover world model para trás para não fazer sombras nos braços
+            thirdPersonModel.transform.position = new Vector3(0f, 0f, -0.5f);
         }
     }
 
@@ -52,15 +56,22 @@ public partial class Player
     
     private void AnimatorUpdate()
     {
+        //TPS Model animator
         thirdPersonAnimator.SetFloat(speedCache, isCrouchedNet.Value ? moveMagnitudeNet.Value : Map(moveMagnitudeNet.Value, 0, 5));
         thirdPersonAnimator.SetFloat(directionCache, Map(Mathf.Atan2(inputXNet.Value, inputYNet.Value) * Mathf.Rad2Deg, -180, 180));
         thirdPersonAnimator.SetBool(crouchCache, isCrouchedNet.Value);
         thirdPersonAnimator.SetBool(jumpCache, !isGroundedNet.Value);
         thirdPersonAnimator.SetFloat(pitchCache, Map(Mathf.Clamp(-xRotationNet.Value, -50, 50), -90, 90));
-        thirdPersonAnimator.SetFloat(yvelCache, Map(velocityYNet.Value, -4, 7)); //velocidade vertical
+        thirdPersonAnimator.SetFloat(yvelCache, Map(velocityYNet.Value, -4, 7));
+
+        //View Model animator
+        firstPersonAnimator.SetBool(sprintCache, isSprinting && input.y > 0); //mostrar braços correndo se estiver correndo apenas para frente
+        firstPersonAnimator.SetBool(crouchCache, isCrouched);
+        firstPersonAnimator.SetFloat(speedCache, isCrouched ? horizontalVelocity.magnitude : Map(horizontalVelocity.magnitude, 0, 5));
+        firstPersonAnimator.SetBool(jumpCache, !isGrounded);
+        firstPersonAnimator.SetFloat(yvelCache, verticalVelocity);
     }
     
-    // Maps a value from some arbitrary range to the 0 to 1 range
     public static float Map(float value, float min, float max)
     {
         return (value - min) * 1f / (max - min);
