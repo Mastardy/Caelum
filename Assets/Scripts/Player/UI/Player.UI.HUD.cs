42497a9a7d64abd846ecde2d124999d909fc2d89
André <andremgmonteiro01@gmail.com>
2022-04-27 18:37:45 +0100

    Added Player Health Dynamic Circular Bar

--- Assets/Scripts/Player/UI/Player.UI.HUD.cs
+++ Assets/Scripts/Player/UI/Player.UI.HUD.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
@@ -6,9 +7,28 @@ public partial class Player
 {
     [Header("HUD")]
     [SerializeField] private TextMeshProUGUI healthText;
+    [SerializeField] private Image healthBackgroundImage;
+    [SerializeField] private Image healthForegroundImage;
 
+    private float lastValue;
+    
     private void HUDUpdate()
     {
+        if (Math.Abs(lastValue - currentHealth.Value / 100f) < 0.001) return;
+        
+        if (lastValue > currentHealth.Value / 100f)
+        {
+            healthForegroundImage.fillAmount = currentHealth.Value / 100f;
+            healthBackgroundImage.fillAmount -= (healthBackgroundImage.fillAmount - healthForegroundImage.fillAmount) * Time.deltaTime * 2;
+            lastValue = healthBackgroundImage.fillAmount;
+        }
+        else
+        {
+            healthBackgroundImage.fillAmount = currentHealth.Value / 100f;
+            healthForegroundImage.fillAmount -= (healthForegroundImage.fillAmount - healthBackgroundImage.fillAmount) * Time.deltaTime * 2;
+            lastValue = healthForegroundImage.fillAmount;
+        }
+        
         healthText.color = Color.Lerp(Color.red, Color.green, currentHealth.Value / 100f);
         healthText.SetText(currentHealth.Value.ToString("F0"));
     }
