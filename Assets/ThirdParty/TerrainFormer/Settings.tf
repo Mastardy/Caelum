f9e3ba45a3cdf0bb2f871d03ddffccd901c7932e
Lucas <lucas_cmattos@hotmail.com.br>
2022-05-02 13:56:05 +0100

    Cont blocagem 1 + pro builder package

---
+++ Assets/ThirdParty/TerrainFormer/Settings.tf
@@ -0,0 +1,345 @@
+{
+    "showSculptingGridPlane": false,
+    "raycastOntoFlatPlane": true,
+    "showSceneViewInformation": true,
+    "displaySceneViewSculptOntoMode": true,
+    "displaySceneViewCurrentTool": true,
+    "displaySceneViewCurrentHeight": true,
+    "brushPreviewSize": 48,
+    "texurePreviewSize": 64,
+    "alwaysShowBrushSelection": false,
+    "alwaysUpdateTerrainLODs": true,
+    "invertBrushTexturesGlobally": false,
+    "cacheSize": 7000,
+    "brushSelectionDisplayType": 2,
+    "fauxModeSettings": [
+        {
+            "tool": 0,
+            "settings": {
+                "useFalloffForCustomBrushes": true,
+                "invertFalloff": false,
+                "selectedBrushTab": "All",
+                "selectedBrushId": "_DefaultFalloffBrushName",
+                "brushSize": 180.0,
+                "brushSpeed": 1.0,
+                "brushRoundness": 1.0,
+                "brushAngle": 0.0,
+                "brushFalloff": {
+                    "serializedVersion": "2",
+                    "m_Curve": [
+                        {
+                            "serializedVersion": "3",
+                            "time": 0.0,
+                            "value": 0.0,
+                            "inSlope": 0.0,
+                            "outSlope": 0.0,
+                            "tangentMode": 0,
+                            "weightedMode": 0,
+                            "inWeight": 0.0,
+                            "outWeight": 0.0
+                        },
+                        {
+                            "serializedVersion": "3",
+                            "time": 1.0,
+                            "value": 1.0,
+                            "inSlope": 0.0,
+                            "outSlope": 1.0,
+                            "tangentMode": 0,
+                            "weightedMode": 0,
+                            "inWeight": 0.0,
+                            "outWeight": 0.0
+                        }
+                    ],
+                    "m_PreInfinity": 2,
+                    "m_PostInfinity": 2,
+                    "m_RotationOrder": 4
+                },
+                "useBrushSpacing": false,
+                "minBrushSpacing": 1.0,
+                "maxBrushSpacing": 30.000001907348634,
+                "useRandomRotation": false,
+                "minRandomRotation": -180.0,
+                "maxRandomRotation": 180.0,
+                "useRandomOffset": false,
+                "randomOffset": 30.0,
+                "invertBrushTexture": false
+            }
+        },
+        {
+            "tool": 2,
+            "settings": {
+                "useFalloffForCustomBrushes": false,
+                "invertFalloff": false,
+                "selectedBrushTab": "All",
+                "selectedBrushId": "_DefaultFalloffBrushName",
+                "brushSize": 35.0,
+                "brushSpeed": 100.0,
+                "brushRoundness": 1.0,
+                "brushAngle": 0.0,
+                "brushFalloff": {
+                    "serializedVersion": "2",
+                    "m_Curve": [
+                        {
+                            "serializedVersion": "3",
+                            "time": 0.0,
+                            "value": 0.0,
+                            "inSlope": 0.0,
+                            "outSlope": 0.0,
+                            "tangentMode": 0,
+                            "weightedMode": 0,
+                            "inWeight": 0.0,
+                            "outWeight": 0.0
+                        },
+                        {
+                            "serializedVersion": "3",
+                            "time": 1.0,
+                            "value": 1.0,
+                            "inSlope": 0.0,
+                            "outSlope": 1.0,
+                            "tangentMode": 0,
+                            "weightedMode": 0,
+                            "inWeight": 0.0,
+                            "outWeight": 0.0
+                        }
+                    ],
+                    "m_PreInfinity": 2,
+                    "m_PostInfinity": 2,
+                    "m_RotationOrder": 4
+                },
+                "useBrushSpacing": false,
+                "minBrushSpacing": 1.0,
+                "maxBrushSpacing": 30.000001907348634,
+                "useRandomRotation": false,
+                "minRandomRotation": -180.0,
+                "maxRandomRotation": 180.0,
+                "useRandomOffset": false,
+                "randomOffset": 30.0,
+                "invertBrushTexture": false
+            }
+        },
+        {
+            "tool": 3,
+            "settings": {
+                "useFalloffForCustomBrushes": false,
+                "invertFalloff": false,
+                "selectedBrushTab": "All",
+                "selectedBrushId": "_DefaultFalloffBrushName",
+                "brushSize": 40.0,
+                "brushSpeed": 98.0,
+                "brushRoundness": 1.0,
+                "brushAngle": -50.0,
+                "brushFalloff": {
+                    "serializedVersion": "2",
+                    "m_Curve": [
+                        {
+                            "serializedVersion": "3",
+                            "time": 0.0,
+                            "value": 0.0,
+                            "inSlope": 0.0,
+                            "outSlope": 0.0,
+                            "tangentMode": 0,
+                            "weightedMode": 0,
+                            "inWeight": 0.0,
+                            "outWeight": 0.0
+                        },
+                        {
+                            "serializedVersion": "3",
+                            "time": 1.0,
+                            "value": 1.0,
+                            "inSlope": 0.0,
+                            "outSlope": 0.0,
+                            "tangentMode": 0,
+                            "weightedMode": 0,
+                            "inWeight": 0.0,
+                            "outWeight": 0.0
+                        }
+                    ],
+                    "m_PreInfinity": 2,
+                    "m_PostInfinity": 2,
+                    "m_RotationOrder": 4
+                },
+                "useBrushSpacing": false,
+                "minBrushSpacing": 1.0,
+                "maxBrushSpacing": 30.000001907348634,
+                "useRandomRotation": false,
+                "minRandomRotation": -180.0,
+                "maxRandomRotation": 180.0,
+                "useRandomOffset": false,
+                "randomOffset": 30.0,
+                "invertBrushTexture": false
+            }
+        },
+        {
+            "tool": 1,
+            "settings": {
+                "useFalloffForCustomBrushes": false,
+                "invertFalloff": false,
+                "selectedBrushTab": "All",
+                "selectedBrushId": "_DefaultFalloffBrushName",
+                "brushSize": 60.0,
+                "brushSpeed": 9.0,
+                "brushRoundness": 1.0,
+                "brushAngle": 0.0,
+                "brushFalloff": {
+                    "serializedVersion": "2",
+                    "m_Curve": [
+                        {
+                            "serializedVersion": "3",
+                            "time": 0.0,
+                            "value": 0.0,
+                            "inSlope": 2.0,
+                            "outSlope": 2.0,
+                            "tangentMode": 0,
+                            "weightedMode": 0,
+                            "inWeight": 0.0,
+                            "outWeight": 0.0
+                        },
+                        {
+                            "serializedVersion": "3",
+                            "time": 1.0,
+                            "value": 1.0,
+                            "inSlope": 0.0,
+                            "outSlope": 0.0,
+                            "tangentMode": 0,
+                            "weightedMode": 0,
+                            "inWeight": 0.0,
+                            "outWeight": 0.0
+                        }
+                    ],
+                    "m_PreInfinity": 2,
+                    "m_PostInfinity": 2,
+                    "m_RotationOrder": 4
+                },
+                "useBrushSpacing": false,
+                "minBrushSpacing": 1.0,
+                "maxBrushSpacing": 30.000001907348634,
+                "useRandomRotation": false,
+                "minRandomRotation": -180.0,
+                "maxRandomRotation": 180.0,
+                "useRandomOffset": false,
+                "randomOffset": 30.0,
+                "invertBrushTexture": false
+            }
+        },
+        {
+            "tool": 4,
+            "settings": {
+                "useFalloffForCustomBrushes": false,
+                "invertFalloff": false,
+                "selectedBrushTab": "All",
+                "selectedBrushId": "_DefaultFalloffBrushName",
+                "brushSize": 35.0,
+                "brushSpeed": 100.0,
+                "brushRoundness": 1.0,
+                "brushAngle": 0.0,
+                "brushFalloff": {
+                    "serializedVersion": "2",
+                    "m_Curve": [
+                        {
+                            "serializedVersion": "3",
+                            "time": 0.0,
+                            "value": 0.0,
+                            "inSlope": 2.0,
+                            "outSlope": 2.0,
+                            "tangentMode": 0,
+                            "weightedMode": 0,
+                            "inWeight": 0.0,
+                            "outWeight": 0.0
+                        },
+                        {
+                            "serializedVersion": "3",
+                            "time": 1.0,
+                            "value": 1.0,
+                            "inSlope": 0.0,
+                            "outSlope": 0.0,
+                            "tangentMode": 0,
+                            "weightedMode": 0,
+                            "inWeight": 0.0,
+                            "outWeight": 0.0
+                        }
+                    ],
+                    "m_PreInfinity": 2,
+                    "m_PostInfinity": 2,
+                    "m_RotationOrder": 4
+                },
+                "useBrushSpacing": false,
+                "minBrushSpacing": 1.0,
+                "maxBrushSpacing": 30.000001907348634,
+                "useRandomRotation": false,
+                "minRandomRotation": -180.0,
+                "maxRandomRotation": 180.0,
+                "useRandomOffset": false,
+                "randomOffset": 30.0,
+                "invertBrushTexture": false
+            }
+        },
+        {
+            "tool": 5,
+            "settings": {
+                "useFalloffForCustomBrushes": false,
+                "invertFalloff": false,
+                "selectedBrushTab": "All",
+                "selectedBrushId": "_DefaultFalloffBrushName",
+                "brushSize": 35.0,
+                "brushSpeed": 100.0,
+                "brushRoundness": 1.0,
+                "brushAngle": 0.0,
+                "brushFalloff": {
+                    "serializedVersion": "2",
+                    "m_Curve": [
+                        {
+                            "serializedVersion": "3",
+                            "time": 0.0,
+                            "value": 0.0,
+                            "inSlope": 2.0,
+                            "outSlope": 2.0,
+                            "tangentMode": 0,
+                            "weightedMode": 0,
+                            "inWeight": 0.0,
+                            "outWeight": 0.0
+                        },
+                        {
+                            "serializedVersion": "3",
+                            "time": 1.0,
+                            "value": 1.0,
+                            "inSlope": 0.0,
+                            "outSlope": 0.0,
+                            "tangentMode": 0,
+                            "weightedMode": 0,
+                            "inWeight": 0.0,
+                            "outWeight": 0.0
+                        }
+                    ],
+                    "m_PreInfinity": 2,
+                    "m_PostInfinity": 2,
+                    "m_RotationOrder": 4
+                },
+                "useBrushSpacing": false,
+                "minBrushSpacing": 1.0,
+                "maxBrushSpacing": 30.000001907348634,
+                "useRandomRotation": false,
+                "minRandomRotation": -180.0,
+                "maxRandomRotation": 180.0,
+                "useRandomOffset": false,
+                "randomOffset": 30.0,
+                "invertBrushTexture": false
+            }
+        }
+    ],
+    "perlinNoiseScale": 20.0,
+    "perlinNoiseMin": 0.0,
+    "perlinNoiseMax": 1.0,
+    "generateRampCurveInXAxis": false,
+    "generateHeight": 930.0,
+    "flattenMode": 1,
+    "setHeight": 185.0,
+    "boxFilterSize": 7,
+    "smoothingIterations": 1,
+    "mouldToolRaycastOffset": 1.7100000381469727,
+    "mouldToolRaycastTopDown": true,
+    "mouldToolBoxFilterSize": 7,
+    "mouldAllIterations": 3,
+    "targetOpacity": 1.0,
+    "selectedTextureIndex": 0,
+    "mainDirectory": "Assets/ThirdParty/TerrainFormer/"
+}
