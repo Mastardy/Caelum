b54ff6872f6ecae36f9fc6fcddb1cb62ef2a6ff6
Lucas <lucas_cmattos@hotmail.com.br>
2022-05-20 18:16:55 +0100

    Updated project settings

--- Assets/Scripts/Resource/ResourceRendererMaster.cs
+++ Assets/Scripts/Resource/ResourceRendererMaster.cs
@@ -1,3 +1,4 @@
+#if UNITY_EDITOR
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -52,3 +53,4 @@ public class ResourceRendererMasterEditor : Editor
         }
     }
 }
+#endif
