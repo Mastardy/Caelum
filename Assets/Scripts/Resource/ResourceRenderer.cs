7dd470294f854c7f30246dc9a55b35da8bd551c8
Lucas <lucas_cmattos@hotmail.com.br>
2022-05-20 17:53:14 +0100

    Updated tree renderer script

--- Assets/Scripts/Resource/ResourceRenderer.cs
+++ Assets/Scripts/Resource/ResourceRenderer.cs
@@ -9,50 +9,34 @@ using UnityEngine.Jobs;
 public class ResourceRenderer : MonoBehaviour
 {
     public bool drawGizmo = true;
-    public bool Place = false;
-    public bool Clear = false;
-    public GameObject[] ResourcePrefab;
+    public Vector3 offset;
+    [Range(-360, 360)] public float rotation = 0;
     public int TreeCount = 10;
+    public int size = 1;
+    public float sizeX = 0.5f;
+    public float sizeZ = 0.5f;
+    [Range(-10,10)]public int seed = 0;
     public int StartHeight = 1000;
     public int lineTraceLength = -1000;
-    public int size = 1;
-    [Range(-10,10)]public int seed = 0;
-    public float distancex = 0.5f;
-    public float distancez = 0.5f;
 
     private List<GameObject> trees = new List<GameObject>();
     private List<Vector3> points = new List<Vector3>();
+    [HideInInspector] public GameObject[] resourcePrefab;
 
-    private void Update()
+    public void PlaceTrees()
     {
-        if (Place)
-        {
-            Place = false;
-            for(int i = 0; i < 5; i++)
-                PlaceTrees();
-        }
-        if (Clear)
-        {
-            Clear = false;
-            ClearTrees();
-        }
-    }
-
-    private void PlaceTrees()
-    {
-        ClearTrees();
-
         Vector3 origin = transform.position;
 
         origin.y = StartHeight;
 
         for (int i = 0; i < points.Count; i++)
         {
-            int rand = Random.Range(0, ResourcePrefab.Length);
-            GameObject tree = Instantiate(ResourcePrefab[rand], transform);
+            int rand = Random.Range(0, resourcePrefab.Length);
+            GameObject tree = Instantiate(resourcePrefab[rand], transform);
             tree.transform.position = points[i];
             tree.transform.Rotate(0, Random.Range(0, 359), 0);
             tree.transform.localScale = Vector3.one * Random.Range(1.9f, 2.3f);
+            tree.name = "Tree";
             trees.Add(tree);
         }
     }
@@ -70,13 +54,14 @@ public class ResourceRenderer : MonoBehaviour
         for (int i = 0; i < TreeCount; i++)
         {
             Vector3 position = origin;
-            position.x += size * Random.Range(-distancex, distancex);
-            position.z += size * Random.Range(-distancez, distancez);
 
-            Vector2 rotatedPoint = RotatePoint(new Vector2(position.x, position.z), new Vector2(transform.position.x, transform.position.z), -transform.rotation.eulerAngles.y);
+            Vector2 randomPoint = new(Random.Range(-sizeX, sizeX), Random.Range(-sizeZ, sizeZ));
+            var angulo = Mathf.Atan2(randomPoint.normalized.y, randomPoint.normalized.x);
 
-            position.x = rotatedPoint.x;
-            position.z = rotatedPoint.y;
+            position.x += Mathf.Cos(angulo - rotation * Mathf.Deg2Rad) * randomPoint.magnitude * size;
+            position.z += Mathf.Sin(angulo - rotation * Mathf.Deg2Rad) * randomPoint.magnitude * size;
+
+            position += offset;
 
             RaycastHit hit;
             if (Physics.Linecast(position, position + new Vector3(0, lineTraceLength, 0), out hit, -LayerMask.NameToLayer("Ground")))
@@ -93,58 +78,40 @@ public class ResourceRenderer : MonoBehaviour
 
         #region BOUNDS
 
-        Vector3 bound1 = transform.position;
-        bound1.x += distancex * size;
-        bound1.z += distancez * size;
-        Vector2 rotatedBound = (RotatePoint(new Vector2(bound1.x, bound1.z), new Vector2(transform.position.x, transform.position.z), -transform.rotation.eulerAngles.y));
-        bound1.x = rotatedBound.x;
-        bound1.z = rotatedBound.y;
+        Vector3[] bounds = new Vector3[4];
 
-        Vector3 bound2 = transform.position;
-        bound2.x += -distancex * size;
-        bound2.z += distancez * size;
-        rotatedBound = (RotatePoint(new Vector2(bound2.x, bound2.z), new Vector2(transform.position.x, transform.position.z), -transform.rotation.eulerAngles.y));
-        bound2.x = rotatedBound.x;
-        bound2.z = rotatedBound.y;
+        for(int i = 0; i < 4; i++)
+        {
+            bounds[i] = transform.position;
 
-        Vector3 bound3 = transform.position;
-        bound3.x += distancex * size;
-        bound3.z += -distancez * size;
-        rotatedBound = (RotatePoint(new Vector2(bound3.x, bound3.z), new Vector2(transform.position.x, transform.position.z), -transform.rotation.eulerAngles.y));
-        bound3.x = rotatedBound.x;
-        bound3.z = rotatedBound.y;
-
-        Vector3 bound4 = transform.position;
-        bound4.x += -distancex * size;
-        bound4.z += -distancez * size;
-        rotatedBound = (RotatePoint(new Vector2(bound4.x, bound4.z), new Vector2(transform.position.x, transform.position.z), -transform.rotation.eulerAngles.y));
-        bound4.x = rotatedBound.x;
-        bound4.z = rotatedBound.y;
+            Vector2 rectangleSize = new(sizeX, sizeZ);
+            rectangleSize.Normalize();
+            var angulo = Mathf.Atan2(rectangleSize.y * (i is 2 or 3 ? -1 : 1), rectangleSize.x * (i is 1 or 2 ? -1 : 1));
+            // Criação do quadrado
+            rectangleSize = new(sizeX, sizeZ);
+            bounds[i].x += Mathf.Cos(angulo - rotation * Mathf.Deg2Rad) * rectangleSize.magnitude * size;
+            bounds[i].z += Mathf.Sin(angulo - rotation * Mathf.Deg2Rad) * rectangleSize.magnitude * size;
+            // Translação do quadrado
+            bounds[i] += offset;
+        }
 
         Gizmos.color = Color.yellow;
-        Gizmos.DrawLine(bound3, bound1);
-        Gizmos.DrawLine(bound2, bound4);
-        Gizmos.DrawLine(bound1, bound2);
-        Gizmos.DrawLine(bound3, bound4);
+        Gizmos.DrawLine(bounds[0], bounds[1]);
+        Gizmos.DrawLine(bounds[1], bounds[2]);
+        Gizmos.DrawLine(bounds[2], bounds[3]);
+        Gizmos.DrawLine(bounds[3], bounds[0]);
         #endregion
     }
 
-    static Vector2 RotatePoint(Vector2 pointToRotate, Vector2 centerPoint, float angleInDegrees)
+    public void ClearTrees()
     {
-        float angleInRadians = angleInDegrees * (Mathf.PI / 180);
-        float cosTheta = Mathf.Cos(angleInRadians);
-        float sinTheta = Mathf.Sin(angleInRadians);
-        return new Vector2((cosTheta * (pointToRotate.x - centerPoint.x) - sinTheta * (pointToRotate.y - centerPoint.y) + centerPoint.x),
-                (sinTheta * (pointToRotate.x - centerPoint.x) + cosTheta * (pointToRotate.y - centerPoint.y) + centerPoint.y));
-    }
+        if (trees.Count == 0) foreach (var tree in GetComponentsInChildren<Transform>()) if(tree.gameObject != gameObject) trees.Add(tree.gameObject);
+        
+        foreach (GameObject tree in trees)
+        {
+            DestroyImmediate(tree.gameObject);
+        }
 
-    private void ClearTrees()
-    {
-        if (trees.Count > 0)
-            foreach (GameObject tree in trees)
-            {
-                DestroyImmediate(tree.gameObject);
-            }
         trees.Clear();
     }
 }
---
+++ Assets/Scripts/Resource/ResourceRendererMaster.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using UnityEditorInternal;
+
+[ExecuteInEditMode]
+public class ResourceRendererMaster : MonoBehaviour
+{
+    private ResourceRenderer[] resources;
+    public GameObject[] resourcePrefab;
+
+    public void Place()
+    {
+        resources = GetComponents<ResourceRenderer>();
+        foreach (var r in resources)
+        {
+            r.resourcePrefab = resourcePrefab;
+            r.PlaceTrees();
+        }
+    }
+
+    public void Clear()
+    {
+        resources = GetComponents<ResourceRenderer>();
+        foreach (var r in resources)
+        {
+            r.ClearTrees();
+        }
+    }
+}
+
+[CustomEditor(typeof(ResourceRendererMaster), true)]
+[CanEditMultipleObjects]
+public class ResourceRendererMasterEditor : Editor
+{
+    private ResourceRendererMaster resourcesMaster;
+    
+    public override void OnInspectorGUI()
+    {
+        resourcesMaster = (ResourceRendererMaster)target;
+
+        base.OnInspectorGUI();
+
+        if (GUILayout.Button(new GUIContent("Place Trees")))
+        {
+            resourcesMaster.Place();
+        }
+        if (GUILayout.Button(new GUIContent("Clear Trees")))
+        {
+            resourcesMaster.Clear();
+        }
+    }
+}
