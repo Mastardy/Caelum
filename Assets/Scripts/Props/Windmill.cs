0d45e3515e57f9e04bd93e49b7f0313b26dd24ac
Lucas <lucas_cmattos@hotmail.com.br>
2022-05-24 16:18:19 +0100

    Added wind vfx and some changes to map and changed tags

--- Assets/Scripts/Props/Windmill.cs
+++ Assets/Scripts/Props/Windmill.cs
@@ -6,17 +6,18 @@ public class Windmill : MonoBehaviour
     private float xRotation;
     [Range(-1,1)] public float speed = 0.2f;
     public float perlinScale = 2;
+    public Vector3 defaultRotation = new Vector3(0, -90, 0);
+    public Vector3 rotationMultiplier = new Vector3(1, 0, 0);
     
     void Start()
     {
         xRotation = Random.Range(0, 359);
-        blades.transform.localRotation = Quaternion.Euler(xRotation,-90,0);
-        speed = Random.Range(-1.0f, -0.2f);
+        blades.transform.localRotation = Quaternion.Euler(xRotation * rotationMultiplier.x, defaultRotation.y + (xRotation * rotationMultiplier.y), defaultRotation.z + (xRotation * rotationMultiplier.z));
     }
     void FixedUpdate()
     {
         xRotation += speed * Mathf.PerlinNoise(0, Time.time / perlinScale) * 2.0f;
-        blades.transform.localRotation = Quaternion.Euler(xRotation,-90,0);
+        blades.transform.localRotation = Quaternion.Euler(xRotation * rotationMultiplier.x, defaultRotation.y + (xRotation * rotationMultiplier.y), defaultRotation.z + (xRotation * rotationMultiplier.z));
         if (xRotation >= 360) xRotation = 0;
     }
 }
