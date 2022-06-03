7f066ef2736d92860305b039e426b0f665f43846
Lucas <lucas_cmattos@hotmail.com.br>
2022-06-03 12:08:10 +0100

    Added fishing animations

--- Assets/Scripts/Entity/FishingNet.cs
+++ Assets/Scripts/Entity/FishingNet.cs
@@ -11,6 +11,12 @@ public class FishingNet : NetworkBehaviour
     [SerializeField] private Vector2 timeRange = new Vector2(5, 10);
     [SerializeField] private Vector2 extraFishTimeRange = new Vector2(1, 3);
 
+    //model variables
+    [SerializeField] private Animator wheelanimator;
+    [SerializeField] private Animator fishanimator;
+    [SerializeField] private Transform tipAnchor;
+    [SerializeField] private GameObject ropeTip;
+
     private bool netLaunched;
     private float launchTime;
     private float fishingTime;
@@ -24,6 +30,7 @@ public class FishingNet : NetworkBehaviour
             if (!netLaunched)
             {
                 netLaunched = true;
+                AnimatorRelease();
                 launchTime = Time.time;
                 fishingTime = Random.Range(timeRange.x, timeRange.y);
                 nextFishTime = fishingTime;
@@ -34,6 +41,8 @@ public class FishingNet : NetworkBehaviour
             if (Time.time - launchTime > fishingTime)
             {
                 netLaunched = false;
+                AnimatorRetrieve();
+                AnimatorCatchFish(false);
                 ply.GiveItemServerRpc(player, "raw_fish", fishesInNet);
                 fishesInNet = 0;
             }
@@ -42,11 +51,31 @@ public class FishingNet : NetworkBehaviour
 
     private void Update()
     {
+        //alterar localização da corda apenas se tiver tocando as animaçoes, e nao em idle
+        if(wheelanimator.GetCurrentAnimatorStateInfo(0).IsName("Wheel_ReleaseKite") || wheelanimator.GetCurrentAnimatorStateInfo(0).IsName("Wheel_RetrieveKite") || wheelanimator.GetCurrentAnimatorStateInfo(0).IsName("Wheel_KiteInAir"))
+            ropeTip.transform.position = tipAnchor.position;
+
         if (!netLaunched) return;
         if (Time.time - launchTime < fishingTime) return;
         if (Time.time - launchTime < nextFishTime) return;
         
         fishesInNet++;
+        AnimatorCatchFish(true);
         nextFishTime += 1 / Random.Range(extraFishTimeRange.x, extraFishTimeRange.y);
+    }
+
+    private void AnimatorRelease()
+    {
+        wheelanimator.SetTrigger("Release");
+    }
+
+    private void AnimatorRetrieve()
+    {
+        wheelanimator.SetTrigger("Retrieve");
+    }
+
+    private void AnimatorCatchFish(bool c)
+    {
+        fishanimator.SetBool("Catch", c);
     }
 }
