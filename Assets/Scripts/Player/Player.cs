b510c7efc79c2773dade70b6bff5007fce229ef7
Lucas <lucas_cmattos@hotmail.com.br>
2022-06-08 22:05:43 +0100

    Fixed some jump animations (read desc)
    while holding tool, sword and grappling hook. Edited player.cs to give grappling hook for testing porpuses

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -79,7 +79,7 @@ public partial class Player : NetworkBehaviour
             GiveItemServerRpc(this, "arrow_wood", 1);
             GiveItemServerRpc(this, "arrow_stone", 1);
             GiveItemServerRpc(this, "arrow_iron", 1);
-            // GiveItemClientRpc("grappling_hook", 1, 1);
+            GiveItemClientRpc(this, "grappling_hook");
             Invoke(nameof(LateLateStart), 0.1f);
         }
     }
