70e1f24923734b60c1d6110cb482aba717c96ded
Lucas <lucas_cmattos@hotmail.com.br>
2022-06-16 16:58:36 +0100

    test

--- Assets/Scripts/Player/Player.Animator.cs
+++ Assets/Scripts/Player/Player.Animator.cs
@@ -67,7 +67,6 @@ public partial class Player
 
     private void EnableFirstPerson()
     {
-        return;
         foreach (var smr in playerViewModelSkinnedMeshRenderers)
         {
             smr.enabled = true;
@@ -83,7 +82,6 @@ public partial class Player
     
     private void DisableFirstPerson()
     {
-        return;
         foreach (var smr in playerViewModelSkinnedMeshRenderers)
         {
             smr.enabled = false;
