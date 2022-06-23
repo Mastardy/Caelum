76f6dad6d33f049e97a74945116c0d2dbf38153f
Lucas <lucas_cmattos@hotmail.com.br>
2022-06-23 17:03:11 +0100

    Added Geysers that deals damage

---
+++ Assets/Scripts/Entity/AirTunnel.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class AirTunnel : MonoBehaviour
+{
+    [SerializeField] private float verticalImpulse = 30f;
+    private float lastImpulse;
+    
+    private void OnTriggerStay(Collider other)
+    {
+        if (!other.CompareTag("Player")) return;
+
+        if (Time.time - lastImpulse > 1.0f)
+        {
+            if (other.GetComponent<Player>().Geyser(verticalImpulse))
+            {
+                lastImpulse = Time.time;                
+            }
+        }
+    }
+}
--- Assets/Scripts/Entity/Geyser.cs
+++ Assets/Scripts/Entity/Geyser.cs
@@ -1,20 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class Geyser : MonoBehaviour
 {
-    [SerializeField] private float verticalImpulse = 30f;
-    private float lastImpulse;
-    
-    private void OnTriggerStay(Collider other)
-    {
-        if (!other.CompareTag("Player")) return;
+    private Player player;
+    public int cameraShakeAmount = 10;
+    public int damage = 25;
+    public ParticleSystem[] particleSystems;
+    public Vector2 burstDelay = new Vector2(10f, 20f);
 
-        if (Time.time - lastImpulse > 1.0f)
+    private void Start()
+    {
+        Invoke("StartPlaying", Random.Range(burstDelay.x, burstDelay.y));
+    }
+
+    private void StartPlaying()
+    {
+        foreach (var ps in particleSystems) ps.Play();
+        if (player)
         {
-            if (other.GetComponent<Player>().Geyser(verticalImpulse))
-            {
-                lastImpulse = Time.time;                
-            }
+            player.TakeDamageServerRpc(damage);
+            player.cameraShake.PlayShake(cameraShakeAmount);
+        }
+
+        Invoke("StartPlaying", Random.Range(burstDelay.x, burstDelay.y));
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            player = other.gameObject.GetComponent<Player>();
         }
     }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            player.cameraShake.StopShake();
+            player = null;
+        }
+    }
+
 }
