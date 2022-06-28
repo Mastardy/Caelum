18af5041d79136e71aecb4c7e5d768e251b873b4
Lucas <lucas_cmattos@hotmail.com.br>
2022-06-28 23:05:23 +0100

    Added render texture

--- Assets/Scripts/Player/PlayerAnimationEvents.cs
+++ Assets/Scripts/Player/PlayerAnimationEvents.cs
@@ -7,8 +7,6 @@ public class PlayerAnimationEvents : MonoBehaviour
     //Este script deve estar contido no objeto que contem o Animator (ex no HumanoidArms dentro do prefab do player)
     [Header("Camera Shake")]
     public CameraShake shake;
-    [Header("Spear")]
-    public GameObject spearModel;
     [Header("Player")]
     public Player player;
 
